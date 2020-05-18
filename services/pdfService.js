f089f389dcca058d0990f86081eaf0d20105ddeb
Yandall <49245907+Yandall@users.noreply.github.com>
2020-05-17 19:45:00 -0500

    Servicio de pdf listo

---
+++ services/pdfService.js
@@ -0,0 +1,37 @@
+const JSReport = require("jsreport-core")();
+const fs = require("fs");
+
+const crearPlantilla = (nombrePlantilla) => {
+  let html = fs.readFileSync(`./templates/${nombrePlantilla}.html`).toString();
+
+  let plantilla = {};
+  plantilla.content = html;
+  plantilla.recipe = "chrome-pdf";
+  plantilla.engine = "handlebars";
+  return plantilla;
+};
+
+const crearPDF = async (data, nombrePlantilla) => {
+  await JSReport.init();
+
+  let infoPdf = {};
+  infoPdf.template = crearPlantilla(nombrePlantilla);
+  infoPdf.data = data;
+
+  let resultado = await JSReport.render(infoPdf);
+  return resultado.content;
+};
+
+module.exports = { crearPDF };
+
+/*crearPDF({publicacion: "Test"}, "publicacionEvaluada")
+.then((res) =>{
+    fs.writeFile("./templates/nuevoPdf.pdf", res, (error) =>{
+        if(error){
+            console.log("error", error)
+        }
+        console.log("documento escrito")
+    })
+}).catch((error) =>{
+    console.log("error", error)
+})*/
