8cc1fbacb4063295c94f62037c164fccfa20384f
alejandro suaza <alejandrosuaza.1022@gmail.com>
2020-05-21 12:22:24 -0400

    send error pdf

--- services/pdfService.js
+++ services/pdfService.js
@@ -3,39 +3,48 @@ const fs = require("fs");
 let jsReportInit = false
 
 const crearPlantilla = (nombrePlantilla) => {
-  let html = fs.readFileSync(`./templates/${nombrePlantilla}.html`).toString();
+    try {
+        let html = fs.readFileSync(`./templates/${nombrePlantilla}.html`).toString();
 
-  let plantilla = {};
-  plantilla.content = html;
-  plantilla.recipe = "chrome-pdf";
-  plantilla.engine = "handlebars";
-  return plantilla;
+        let plantilla = {};
+        plantilla.content = html;
+        plantilla.recipe = "chrome-pdf";
+        plantilla.engine = "handlebars";
+        return plantilla;
+    } catch (error) {
+        console.log(error);
+
+    }
 };
 
-const crearPDF = async (req, res) => {
+const crearPDF = async(req, res) => {
 
-  let data = req.body.data
-  let nombrePlantilla = req.body.template
-  if(!jsReportInit){
-    jsReportInit = true
-    await JSReport.init();
-  }
-  
+    try {
+        let data = req.body.data
+        let nombrePlantilla = req.body.template
+        if (!jsReportInit) {
+            jsReportInit = true
+            await JSReport.init();
+        }
 
-  let infoPdf = {};
-  infoPdf.template = crearPlantilla(nombrePlantilla);
-  infoPdf.data = data;
 
-  let resultado = await JSReport.render(infoPdf);
+        let infoPdf = {};
+        infoPdf.template = crearPlantilla(nombrePlantilla);
+        infoPdf.data = data;
 
-  fs.writeFile("./files/nuevoPdf.pdf", resultado.content, (error) =>{
-    if(error){
-        console.log("error", error)
+        let resultado = await JSReport.render(infoPdf);
+
+        fs.writeFile("./files/nuevoPdf.pdf", resultado.content, (error) => {
+            if (error) {
+                console.log("error", error)
+            }
+            console.log("documento escrito")
+        })
+
+        res.status(200).send({ ok: true, mensaje: "Pdf creado" })
+    } catch (error) {
+        res.send(error)
     }
-    console.log("documento escrito")
-  })
-
-  res.status(200).send({ok: true, mensaje:"Pdf creado"})
 };
 
 module.exports = { crearPDF };
